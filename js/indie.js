de91c3d32c0cdfad67facc2c06de0bae74c836f7
IAMSAM <chortletortle@gmail.com>
2015-04-16 09:53:31 -0400

    moved files haha

---
+++ js/indie.js
@@ -0,0 +1,6 @@
+var elem = document.getElementById('draw-animation').children[0];
+var two = new Two({ width: window.innerWidth, height: window.innerHeight }).appendTo(elem);
+
+var square = two.makeRectangle(two.width/2, two.height/2, 100, 100);
+var line1 = 
+var lines = [];
